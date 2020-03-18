669e52c59597f7a110ee0a99709d72a652dd1098
Axsiow <TehSiow2@gmail.com>
2020-03-18 14:05:44 +0100

    login et singup

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/auth';
+// import { AngularFireDatabase } from '@angular/fire/database';
 
 @Component({
 	selector: 'app-login',
@@ -8,18 +9,64 @@ import { AngularFireDatabase } from '@angular/fire/database';
 })
 export class LoginPage implements OnInit {
 
-	constructor(
-		public AfDB: AngularFireDatabase
-	) { }
+		// Declaration :
+	
+	dataUser = {
+		email: '',
+		password: ''
+	};
+	connected: boolean;
 
-	add() {
-		this.AfDB.list('User/').push({
-			pseudo: 'okboomer'
+	userId: string;
+	mail: string;
+	method: any;
+
+	constructor(
+		//public toastController: ToastController,
+		//public afDB: AngularFireDatabase,
+		public afAuth: AngularFireAuth
+	) { 
+		this.afAuth.authState.subscribe(auth => {
+			if (!auth) {
+				console.log('non connecté');
+				this.connected = false;
+			} else {
+				console.log('connecté: ' + auth.uid);
+				this.connected = true;
+				this.userId = auth.uid;
+				this.mail = auth.email;
+				this.method = auth.providerData[0].providerId;
+			}
 		});
-		console.log("fdp");
-		}
+	}
 
 	ngOnInit() {
 	}
 
+		// Connection
+
+	login() {
+		this.afAuth.auth.signInWithEmailAndPassword(this.dataUser.email, this.dataUser.password);
+		 this.dataUser = {
+		   email: '',
+		   password: ''
+		 };
+	}
+
+		// Inscription
+
+	signUp() {
+		this.afAuth.auth.createUserWithEmailAndPassword(this.dataUser.email, this.dataUser.password);
+		this.dataUser = {
+		  email: '',
+		  password: ''
+		};
+	}
+
+	// Deconection
+
+	logout() {
+		this.afAuth.auth.signOut();
+	}
+	
 }
